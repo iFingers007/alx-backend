3f7199ae598d493443f0430f99ef63b8f4ba678b
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-28 17:40:11 +0100

    Fixed code

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Module for 0. Simple helper function"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Returns range of a page
+    Args:
+    page: Page Number
+    page_size: Size of the page
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a paginated page of the dataset
+        """
+        assert isinstance(page, int) and page > 0, "Page must be > 0"
+        assert isinstance(page_size, int) and page_size > 0, "Size must be > 0"
+
+        start, end = index_range(page, page_size)
+
+        data = self.dataset()
+        return data[start:end] if start < len(data) else []
