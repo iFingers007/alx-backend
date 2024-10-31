3cede5d48b8e2dd2fcfa256cdc71afcea6c19692
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-31 01:29:36 +0100

    Fixed code

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
-"""LIFOCache Module
+"""LRUcache Module
 """
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
-class LIFOCache(BaseCaching):
-    """Defines a caching system with LIFO algorithm
+class LRUCache(BaseCaching):
+    """Defines a caching system with LRU algorithm
     """
     def __init__(self):
         """Initialising of the class"""
         super().__init__()
-        self.keys = []
+        self.cached_data = OrderedDict()
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item value for the key
         """
         if key is not None and item is not None:
-            if key in self.cache_data:
-                self.keys.remove(key)
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                popped_key, _ = self.keys.pop()
-                print(f"DISCARD: {popped_key}")
-                del self.cache_data[popped_key]
-            self.keys.append(key)
+            if key in self.order:
+                del self.order[key]
+            self.order[key] = item
             self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                popped_key, _ = self.order.popitem(last=False)
+                del self.cache_data[popped_key]
+                print(f"DISCARD: {popped_key}")
 
     def get(self, key):
         """returns value linked to key"""
-        return self.cache_data.get(key)
+        if key in self.cache_data:
+            item = self.cahe_data.pop(key)
+            self.cache_data[key] = item
+            return item
+        return None
+
